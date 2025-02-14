0cbd232fcdd570afe7f9a6cdfc8d4824b5fef131
Symom Hossain Shohan <shshohan0000@gmail.com>
2025-02-14 16:43:34 +0600

    Implement quicksort and mergesort algorithms with modular arithmetic support

--- StartAnew/mergesort.cpp
+++ StartAnew/mergesort.cpp
@@ -1,77 +1,110 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-
-
-ll Merge(vector<ll> &L, vector<ll> &R, vector<ll> &v)
+typedef long long ll;
+const ll mod = 1e9 + 7;
+const ll MX = 2e5 + 5;
+inline void norm(ll &a)
 {
-    int i=0, j=0, k=0, cnt = 0;  
-    while(i<(int)L.size() and j<(int)R.size())
+    a %= mod;
+    (a < 0) && (a += mod);
+} // positive mod value
+inline ll modAdd(ll a, ll b)
+{
+    a %= mod, b %= mod;
+    norm(a), norm(b);
+    return (a + b) % mod;
+} // modular addition
+inline ll modSub(ll a, ll b)
+{
+    a %= mod, b %= mod;
+    norm(a), norm(b);
+    return (a - b) % mod;
+} // modular subtraction
+inline ll modMul(ll a, ll b)
+{
+    a %= mod, b %= mod;
+    norm(a), norm(b);
+    return (a * b) % mod;
+} // modular multiplication
+inline ll bigMod(ll b, ll p)
+{
+    ll r = 1;
+    while (p)
     {
-        if(L[i]<=R[j])
-        {
-            v[k] = L[i]; 
-            k++, i++;  
-            
-            
-        }
-        else 
-        {
-            v[k] = R[j];  
-            k++, j++;  
-            cnt += L.size()-i; 
-        }
-        
+        if (p & 1LL)
+            r = modMul(r, b);
+        b = modMul(b, b);
+        p >>= 1LL;
     }
-    while(i<(int)L.size())
+    return r;
+}
+inline ll modInverse(ll a) { return bigMod(a, mod - 2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+void merge(vector<ll> &v, ll start, ll mid, ll end)
+{
+    vector<ll> temp;
+
+    ll i = start, j = mid + 1;
+    while (i <= mid and j <= end)
     {
-        v[k] = L[i]; 
-        i++, k++; 
-          
-    }
-    while(j<(int)R.size())
-    {
-        v[k] = R[j];  
-        j++, k++;
-        
+        if (v[i] <= v[j])
+        {
+            temp.push_back(v[i]);
+            i++;
+        }
+        else
+        {
+            temp.push_back(v[j]);
+            j++;
+        }
     }
 
-    return cnt; 
+    while (i <= mid)
+    {
+        temp.push_back(v[i]);
+        i++;
+    }
+    while (j <= end)
+    {
+        temp.push_back(v[j]);
+        j++;
+    }
+
+    for (int idx = 0; idx < temp.size(); idx++)
+    {
+        v[idx + start] = temp[idx];
+    }
 }
 
-ll mergesort(vector<ll> &v)
+void mergeSort(vector<ll> &v, ll start, ll end)
 {
-    ll n = v.size();
-    ll cnt = 0; 
-    
-    if (n < 2)
+    if (start < end)
     {
-        //cnt++; 
-         return 0;
+        ll mid = (start + end) / 2;
+        mergeSort(v, start, mid);
+        mergeSort(v, mid + 1, end);
+        merge(v, start, mid, end); 
     }
-    ll m = n / 2;
-    vector<ll> L(m), R(n - m);
-    for (int i = 0; i < m; i++)
-        L[i] = v[i];
-    for (int i = m, j = 0; i < n; i++, j++)
-        R[j] = v[i];
-    
-    cnt += mergesort(L); 
-    cnt += mergesort(R); 
-    cnt += Merge(L, R, v); 
+}
 
-    return cnt; 
+void solve()
+{
+    vector<ll> v = {6, 4, 5, 1, 2}; 
+    mergeSort(v, 0, v.size()-1); 
+    for(auto i: v) cout << i << endl; 
 }
 
 int main()
 {
-    ll n;
-    cin >> n;
-    vector<ll> v(n);
-    for (auto &i : v)
-        cin >> i;
-    cout << mergesort(v) << endl; 
-    for(auto i: v) cout << i << ' '; 
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll tc = 1;
+    // cin >> tc;
+    for (ll t = 1; t <= tc; t++)
+    {
+        solve();
+    }
+    return 0;
 }
--- StartAnew/quicksort.cpp
+++ StartAnew/quicksort.cpp
@@ -1,38 +1,60 @@
-#include<bits/stdc++.h>
-using namespace std; 
-#define ll long long 
+#include <bits/stdc++.h>
+using namespace std;
 
-ll Partition(vector<ll> &v, ll start, ll end)
+typedef long long ll;
+const ll mod=1e9+7;
+const ll MX=2e5+5;
+inline void norm(ll &a) {a%=mod; (a<0) && (a+=mod) ;}                            //positive mod value
+inline ll modAdd(ll a,ll b) {a%=mod, b%=mod; norm(a),norm(b); return (a+b)%mod;} //modular addition
+inline ll modSub(ll a,ll b) {a%=mod, b%=mod; norm(a),norm(b); return (a-b)%mod;} //modular subtraction
+inline ll modMul(ll a,ll b) {a%=mod, b%=mod; norm(a),norm(b); return (a*b)%mod;} //modular multiplication
+inline ll bigMod(ll b,ll p)  {ll r=1; while(p) {if(p & 1LL) r=modMul(r,b) ;b=modMul(b,b) ; p>>=1LL ; } return r; }
+inline ll modInverse(ll a) {return bigMod(a,mod-2); }
+inline ll modDiv(ll a ,ll b) { return modMul(a,modInverse(b)) ;}
+
+
+ll partition(vector<ll> &v, ll st, ll end)
 {
-    ll pix = start, pivot = end; 
-    for(int i = start; i<end; i++)
+    ll idx = st-1, pivot = v[end]; 
+    for(int j=st;j<end; j++)
     {
-        if(v[i]<v[pivot]) 
+        if(v[j]<pivot)
         {
-            swap(v[i], v[pix]); 
-            pix++;  
+            idx++; 
+            swap(v[j], v[idx]); 
         }
     }
-    swap(v[pivot], v[pix]); 
-    return pix;  
+    idx++; 
+    swap(v[idx], v[end]); 
+    return idx; 
 }
 
-void quicksort(vector<ll> &v, ll start, ll end)
+void quickSort(vector<ll> &v, ll st, ll end)
 {
-    if(start<end)
+    if(st<end)
     {
-        ll ix = Partition(v, start, end);  
-        quicksort(v, start, ix-1); 
-        quicksort(v, ix+1, end);  
+        ll pivIndx = partition(v, st, end); 
+        quickSort(v, st, pivIndx-1); 
+        quickSort(v, pivIndx+1, end); 
     }
+}
+
+void solve()
+{
+    vector<ll> v = {4, 2, 5, 1, 3}; 
+    quickSort(v, 0, v.size()-1); 
+    for(auto i: v) cout << i << endl; 
 }
 
 int main()
 {
-    ll n; 
-    cin >> n; 
-    vector<ll> v(n);  
-    for(auto &i: v) cin >> i; 
-    quicksort(v, 0, n-1);  
-    for(auto i: v) cout << i << ' ';  
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll tc = 1;
+    //cin >> tc;
+    for (ll t = 1; t <= tc; t++)
+    {
+        solve();
+    }
+    return 0;
 }
